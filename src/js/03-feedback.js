7f69834a9b8585cc24bedcf9b302e84f6544549b
LudmilaAliarova <91786435+LudmilaAliarova@users.noreply.github.com>
2022-04-08 14:39:47 +0300

    Update-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,18 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-// отслеживаем INPUT и обновляем хранилище не чаще чем раз в 500 мс
+
 form.addEventListener('input', throttle(inputData, 500));
 form.addEventListener('submit', submitForm);
 
 dataLocalStorage();
 
 const formData = {};
-// при вводе данных записываем их в объект FormData
 function inputData(evt) {
   formData[evt.target.name] = evt.target.value;
-  const dataJson = JSON.stringify(formData);
-  localStorage.setItem('feedback-form-state', dataJson);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   console.log(formData);
 }
-// очищение полей формы и хранилища, вывод в консоль
 
 function submitForm(evt) {
   evt.preventDefault();
@@ -26,14 +23,12 @@ function submitForm(evt) {
 
 function dataLocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  //проверка формы
+  //console.log(data);
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
-  // заполнение полей при перезагрузке страницы
-  if (data.email) {
+
+  if (data) {
     email.value = data.email;
-  }
-  if (data.message) {
     message.value = data.message;
   }
 }
